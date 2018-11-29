2a6c7ea3d86bebd284754bb8ab5c791d610cb9d9
gpuhub <gpuhub@gpuhub.org>
2018-11-29 15:55:35 +0300

    Add files

---
+++ js/jquery.backtotop.js
@@ -0,0 +1,1 @@
+(function($){$.fn.backToTop=function(){var selectz=$(this);selectz.hide();$(window).scroll(function(){if($(this).scrollTop()>$(window).height()){selectz.fadeIn();}else{selectz.fadeOut();}});};}(jQuery));
