9eafd0d1c77e5bd00b0afcb7aeb5afdc23536151
gpuhub <gpuhub@gpuhub.org>
2018-12-07 02:16:08 +0300

    Revert several dependencie

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var dp=jQuery;
 
 dp(document).ready(function() {
     Grid.init();
+    dp('.bg-about').parallax("10%", 1);
     dp("#backtotop").backToTop();
     var videobackground = new dp.backgroundVideo(dp('.home-video'), {
         "align": "centerXY",
@@ -18,5 +19,12 @@ dp(document).ready(function() {
         dp(this).removeClass('animatez');
         dp(this).addClass('animated ' + effect);
     }, {offset: '70%'});
-
+    dp(".sscroll").smoothScroll();
+    dp("#sscroll_contact").smoothScroll({offset: 50});
+    dp(".responsive-video").fitVids();
+    if (dp.fn.backstretch) {
+        var bg_image = dp(".home-image");
+        var bg_image_src = bg_image.data("src");
+        bg_image.backstretch(bg_image_src);
+    }
 });
